93e2814ef4073b7a02a222872c79d45c3d800622
majkprpik <vedran.prpic1@gmail.com>
2018-11-10 13:33:39 +0100

    cors added

--- server/server.js
+++ server/server.js
@@ -1,45 +1,55 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
-const passport = require("passport")
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const users = require("./routes/api/users")
-const roles = require("./routes/api/roles")
-const shifts = require("./routes/api/shifts")
-const schedules = require("./routes/api/schedules")
-const permissions = require("./routes/api/permissions")
+const users = require("./routes/api/users");
+const roles = require("./routes/api/roles");
+const shifts = require("./routes/api/shifts");
+const schedules = require("./routes/api/schedules");
+const permissions = require("./routes/api/permissions");
 
-
-const app = express()
+const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB config
-const db = require("./config/keys").mongoURI
+const db = require("./config/keys").mongoURI;
 
 //Connect to MongoDB
 mongoose
   .connect(db)
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
 // Passport middleware
-app.use(passport.initialize())
+app.use(passport.initialize());
 
 // Passport config
-require("./config/passport")(passport)
+require("./config/passport")(passport);
+
+//only for development purpose
+var originsWhitelist = ["http://localhost:4200"];
+var corsOptions = {
+  origin: function(origin, callback) {
+    var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+    callback(null, isWhitelisted);
+  },
+  credentials: true
+};
+app.use(cors(corsOptions));
 
 // Use routes
-app.use("/users", users)
-app.use("/roles", roles)
-app.use("/shifts", shifts)
-app.use("/schedules", schedules)
-app.use("/permissions", permissions)
+app.use("/users", users);
+app.use("/roles", roles);
+app.use("/shifts", shifts);
+app.use("/schedules", schedules);
+app.use("/permissions", permissions);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}!`)
-})
+  console.log(`App listening on port ${port}!`);
+});
