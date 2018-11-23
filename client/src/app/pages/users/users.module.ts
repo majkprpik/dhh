cf1cdaac653fbba7fd4abf2641e0e83001cfe31a
majkprpik <vedran.prpic1@gmail.com>
2018-11-23 18:01:20 +0100

    fix _role

--- client/src/app/pages/users/users.module.ts
+++ client/src/app/pages/users/users.module.ts
@@ -7,14 +7,14 @@ import { ThemeModule } from '../../@theme/theme.module';
 import { NbCardModule } from '@nebular/theme';
 
 import { Ng2SmartTableModule } from 'ng2-smart-table';
-
+import { RolesService } from '../../services/roles/roles.service';
 import { TablesRoutingModule, routedComponents } from './users-routing.module';
 import { SmartTableService } from '../../@core/data/smart-table.service';
 @NgModule({
   imports: [ThemeModule, NgxEchartsModule, NbCardModule, TablesRoutingModule,
     Ng2SmartTableModule],
   declarations: [...routedComponents, UsersComponent], providers: [
-    SmartTableService,
+    SmartTableService, RolesService,
   ],
 })
 export class UsersModule { }
